d920f1709ec4448484757e3075a6838b335c5bd2
Manish <mkjha9555@gmail.com>
2017-12-14 16:15:46 +0530

    add width and height

--- handler.js
+++ handler.js
@@ -6,41 +6,39 @@ const S3 = new AWS.S3({
   signatureVersion: 'v4',
 });
 const BUCKET = "wvalpha";
-const URL =  "https://d8rwe65c9cuak.cloudfront.net/uploads/uploads/versions/";
-// import { Base64 } from 'js-base64';
-//ImageResizer Function
-
+//const URL =  "https://d8rwe65c9cuak.cloudfront.net/uploads/uploads/versions/";
+ const URL =  "https://s3.ap-south-1.amazonaws.com/wvalpha/uploads/uploads/versions/";
 module.exports.imageResize = async  (event, context, callback) => {
-context.callbackWaitsForEmptyEventLoop = false;
-const id = event.pathParameters.key;
-const key = id;
-const width =event.pathParameters.width;
-const originalKey =id;
-S3.getObject({Bucket: BUCKET, Key: originalKey}).promise()
+    context.callbackWaitsForEmptyEventLoop = false;
+    const key = event.pathParameters.key;
+    const data =event.pathParameters.data;
+    let params  = decodeURIComponent(event.pathParameters.data);
+    let pathParams =params.split(",");
+    let finalWidth= pathParams[2].split("_");
+    let finalHeight= pathParams[3].split("_");
+    finalWidth=parseInt(finalWidth[1]);
+    finalHeight=parseInt(finalHeight[1]);
+S3.getObject({Bucket: BUCKET, Key: key}).promise()
   .then(data => 
-      Sharp(data.Body)
-    .resize(parseInt(width))
-    .toFormat('png')
+     Sharp(data.Body)
+    .resize(finalWidth ,finalHeight) 
     .toBuffer()
   )
   .then(buffer => S3.putObject({
       Body: buffer,
-      Bucket: BUCKET+"/uploads/uploads/versions/"+(width),
+      Bucket: BUCKET+"/uploads/uploads/versions/"+finalWidth,
       ContentType: 'image/png',
       Key: key,
       ACL:"public-read"
     }).promise()
   )
   .then((data) => 
-  
-  
-  
   callback(null, {
     statusCode: 200,
     headers: {
         'Content-Type': 'text/html'
     },
-      body:'<html><head><meta name="viewport" content="width=device-width, minimum-scale=0.1"><title>' + URL+width+"/"+key + '</title></head><body style="margin: 0px; background: #0e0e0e;text-align: center;"><img style="-webkit-user-select: none;background-position: 0px 0px, 10px 10px;background-size: 20px 20px;background-image:linear-gradient(45deg, #eee 25%, transparent 25%, transparent 75%, #eee 75%, #eee 100%),linear-gradient(45deg, #eee 25%, white 25%, white 75%, #eee 75%, #eee 100%);text-align: center;height: auto;" src="' + URL+width+"/"+key + '"></body></html>'
+      body:'<html><head><meta name="viewport" content="width=device-width, minimum-scale=0.1"><title>' + URL+finalWidth+"/"+key + '</title></head><body style="background:#000;"><img style="height: auto;max-height:100vh;position: absolute;left: 0;right: 0;top: 0;bottom: 0;margin: auto;" src="' + URL+finalWidth+"/"+key + '" alt=""></body></html>'
       
     })
   )
