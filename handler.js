df9e493c4229408215e429131a640fb4a331683e
Manish <mkjha9555@gmail.com>
2017-12-14 11:46:50 +0530

    add file rendering

--- handler.js
+++ handler.js
@@ -7,7 +7,7 @@ const S3 = new AWS.S3({
 });
 const BUCKET = "wvalpha";
 const URL =  "https://d8rwe65c9cuak.cloudfront.net/uploads/uploads/versions/";
-
+// import { Base64 } from 'js-base64';
 //ImageResizer Function
 
 module.exports.imageResize = async  (event, context, callback) => {
@@ -31,11 +31,16 @@ S3.getObject({Bucket: BUCKET, Key: originalKey}).promise()
       ACL:"public-read"
     }).promise()
   )
-  .then((data) => callback(null, {
-      statusCode: '301',
-      header:
-        "https://d8rwe65c9cuak.cloudfront.net/uploads/uploads/versions/"+width+"/"+key+"",
-      body:URL+width+"/"+key
+  .then((data) => 
+  
+  
+  
+  callback(null, {
+    statusCode: 200,
+    headers: {
+        'Content-Type': 'text/html'
+    },
+      body:'<html><head><meta name="viewport" content="width=device-width, minimum-scale=0.1"><title>' + URL+width+"/"+key + '</title></head><body style="margin: 0px; background: #0e0e0e;text-align: center;"><img style="-webkit-user-select: none;background-position: 0px 0px, 10px 10px;background-size: 20px 20px;background-image:linear-gradient(45deg, #eee 25%, transparent 25%, transparent 75%, #eee 75%, #eee 100%),linear-gradient(45deg, #eee 25%, white 25%, white 75%, #eee 75%, #eee 100%);text-align: center;height: auto;" src="' + URL+width+"/"+key + '"></body></html>'
       
     })
   )
