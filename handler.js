089f25b1ebd5f0a0a8aceac75521575334372b8d
Manish <mkjha9555@gmail.com>
2017-12-14 16:11:32 +0530

    add files

--- handler.js
+++ handler.js
@@ -2,47 +2,89 @@
 const bluebird = require('bluebird');
 const AWS = require('aws-sdk');
 const Sharp = require('sharp');
-const S3 = new AWS.S3({
+const s3 = new AWS.S3({
   signatureVersion: 'v4',
 });
 const BUCKET = "wvalpha";
 const URL =  "https://d8rwe65c9cuak.cloudfront.net/uploads/uploads/versions/";
-// import { Base64 } from 'js-base64';
-//ImageResizer Function
-
-module.exports.imageResize = async  (event, context, callback) => {
-context.callbackWaitsForEmptyEventLoop = false;
-const id = event.pathParameters.key;
-const key = id;
-const width =event.pathParameters.width;
-const originalKey =id;
-S3.getObject({Bucket: BUCKET, Key: originalKey}).promise()
-  .then(data => 
-      Sharp(data.Body)
-    .resize(parseInt(width))
-    .toFormat('png')
-    .toBuffer()
-  )
-  .then(buffer => S3.putObject({
-      Body: buffer,
-      Bucket: BUCKET+"/uploads/uploads/versions/"+(width),
-      ContentType: 'image/png',
-      Key: key,
-      ACL:"public-read"
-    }).promise()
-  )
-  .then((data) => 
-  
-  
-  
-  callback(null, {
-    statusCode: 200,
-    headers: {
-        'Content-Type': 'text/html'
-    },
-      body:'<html><head><meta name="viewport" content="width=device-width, minimum-scale=0.1"><title>' + URL+width+"/"+key + '</title></head><body style="margin: 0px; background: #0e0e0e;text-align: center;"><img style="-webkit-user-select: none;background-position: 0px 0px, 10px 10px;background-size: 20px 20px;background-image:linear-gradient(45deg, #eee 25%, transparent 25%, transparent 75%, #eee 75%, #eee 100%),linear-gradient(45deg, #eee 25%, white 25%, white 75%, #eee 75%, #eee 100%);text-align: center;height: auto;" src="' + URL+width+"/"+key + '"></body></html>'
-      
-    })
-  )
-  .catch(err => callback(err))
+module.exports.imageResize = async function(event, context, callback) {
+  try {
+    const id = event.pathParameters.key;
+    const originalKey =id;
+    const resizeWidth =event.pathParameters.width;
+      await getFileFromS3AndContinue(originalKey,resizeWidth,callback);
+  } catch (errorMessage) {
+     await  returnErrorResponse(errorMessage, callback);
+  }
 };
+function returnErrorResponse(errorMessage, callback) {
+  const response = {
+      statusCode: 400,
+      body: 'Error: ' + JSON.stringify(errorMessage)
+  };
+
+  callback(null, response);
+}
+function getFileFromS3AndContinue(originalKey,resizeWidth, callback) {
+  try {
+      s3.getObject({Bucket: BUCKET, Key: originalKey}, (err, result) => {
+         if(err) {
+            returnErrorResponse(err, callback);
+          } else {
+              resizeAndContinue(result.Body, resizeWidth, callback);
+          }
+      });
+  } catch (errorMessage) {
+      returnErrorResponse(errorMessage, callback);
+  }
+}
+
+ function resizeAndContinue(content, resizeWidth, callback) {
+  try {
+    const width = resizeWidth;
+    if(width > 0 && width < 10000) {
+          const image = Sharp(content).resize(parseInt(width)).toFormat('png')  ;
+        image.toBuffer((err, buffer, info) => {
+            
+                  if(err) {
+                    returnErrorResponse(err, callback);
+                  } else {
+                    returnResponse(buffer,callback);
+                  }
+              });
+      } else {
+          returnResponse(content,callback);
+      }
+  } catch (errorMessage) {
+      returnErrorResponse(errorMessage, callback);
+  }
+}
+function returnResponse(content,callback) {
+  try {
+      const body = content.toString("base64");
+      const MAX_BASE64_LAMBDA_RESPONSE_SIZE = 4718628; // Based on my testing as of 2016-12-30
+      if(body.length > MAX_BASE64_LAMBDA_RESPONSE_SIZE) {
+          throw `Response body size ${body.length} bytes is greater than the limit of ${MAX_BASE64_LAMBDA_RESPONSE_SIZE} bytes`;
+      }
+      var data = "<img height=100 width=100 src='data:image/png;base64,"+body+"' />";
+      const response = {
+          statusCode: 200,
+          headers: {
+              "Content-Type": "image/png"
+          },
+          body:data,
+          isBase64Encoded: true
+      };
+     
+
+      callback(null, response);
+  } catch (errorMessage) {
+      returnErrorResponse(errorMessage, callback);
+  }
+}
+
+
+
+
+
+
