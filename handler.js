83be85b1cf326d5cfffbc83696dd45667f449585
Manish <mkjha9555@gmail.com>
2017-12-14 19:47:14 +0530

    add condition for image

--- handler.js
+++ handler.js
@@ -6,7 +6,9 @@ const S3 = new AWS.S3({
   signatureVersion: 'v4',
 });
 const BUCKET = "wvalpha";
+
 //const URL =  "https://d8rwe65c9cuak.cloudfront.net/uploads/uploads/versions/";
+
  const URL =  "https://s3.ap-south-1.amazonaws.com/wvalpha/uploads/uploads/versions/";
 module.exports.imageResize = async  (event, context, callback) => {
     context.callbackWaitsForEmptyEventLoop = false;
@@ -14,15 +16,56 @@ module.exports.imageResize = async  (event, context, callback) => {
     const data =event.pathParameters.data;
     let params  = decodeURIComponent(event.pathParameters.data);
     let pathParams =params.split(",");
-    let finalWidth= pathParams[2].split("_");
-    let finalHeight= pathParams[3].split("_");
-    finalWidth=parseInt(finalWidth[1]);
-    finalHeight=parseInt(finalHeight[1]);
+    let finalWidth,finalHeight,c_fill,c_fit;
+    if(pathParams.length===3){
+        if(pathParams[0]==="c_fill"){
+            c_fill="max";
+            c_fit=null;
+        }
+        else if (pathParams[0]==="c_fit"){
+            c_fit="min";
+            c_fill=null;
+        }
+        finalWidth= pathParams[1].split("_");
+        finalHeight= pathParams[2].split("_");
+        finalWidth=parseInt(finalWidth[1]);
+        finalHeight=parseInt(finalHeight[1]);
+    }
+    else if(pathParams.length===2){
+        finalWidth= pathParams[0].split("_");
+        finalHeight= pathParams[1].split("_");
+        finalWidth=parseInt(finalWidth[1]);
+        finalHeight=parseInt(finalHeight[1]);
+    }
+    else if(pathParams.length===1){
+        let getValue = pathParams[0].split("_")[0];
+        
+        if(getValue==="h"){
+            finalHeight=parseInt(pathParams[0].split("_")[1]);
+            finalWidth=null
+        }
+        else if(getValue==="w"){
+            finalWidth=parseInt(pathParams[0].split("_")[1]);
+            finalHeight=null
+        }
+    }
 S3.getObject({Bucket: BUCKET, Key: key}).promise()
-  .then(data => 
-     Sharp(data.Body)
-    .resize(finalWidth ,finalHeight) 
-    .toBuffer()
+  .then((data) => {
+      console.log(c_fill)
+      console.log(pathParams.length);
+    if(c_fill==="max" && pathParams.length == 3){
+        console.log("here in max");
+        return Sharp(data.Body).resize(finalWidth ,finalHeight).max().toBuffer()
+    }
+     else if(c_fit==="min" && pathParams.length == 3){
+        console.log("here in min");
+        return Sharp(data.Body).resize(finalWidth ,finalHeight).min().toBuffer()
+    }
+     else if(c_fit!== "min" && c_fill !== "min"  &&  pathParams.length <=2) {
+        console.log("here no min no max");
+        return Sharp(data.Body).resize(finalWidth ,finalHeight).toBuffer()
+    }
+  }
   )
   .then(buffer => S3.putObject({
       Body: buffer,
